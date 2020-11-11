5299f7691657cf9d5b24dbd9050e7dd34b6ce20f
Dan <danielfr500@gmail.com>
2020-11-10 20:12:30 -0500

    small changes, file name changes

--- Weather.py
+++
@@ -1,7 +0,0 @@
-import api
-import requests
-
-def getWeather(cityState):
-    response = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}".format(cityState, api.openWeatherApi))
-    return [ response.status_code, cityState, response.json()]
-
